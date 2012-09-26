594c93348addd7bf6707516ec767f0a9cbc9135b
Michael Mior <mike@joinbunch.com>
2012-09-26 10:24:11 -0400

    Publicly expose defaultHeaders from AFHTTPClient

--- AFNetworking/AFHTTPClient.h
+++ AFNetworking/AFHTTPClient.h
@@ -160,6 +160,11 @@ extern NSString * AFQueryStringFromParametersWithEncoding(NSDictionary *paramete
 @property (readonly, nonatomic, retain) NSOperationQueue *operationQueue;
 
 /**
+ Default headers sent on every request by the HTTP client.
+ */
+@property (readonly, nonatomic, retain) NSMutableDictionary *defaultHeaders;
+
+/**
  The reachability status from the device to the current `baseURL` of the `AFHTTPClient`.
 
   @warning This property requires the `SystemConfiguration` framework. Add it in the active target's "Link Binary With Library" build phase, and add `#import <SystemConfiguration/SystemConfiguration.h>` to the header prefix of the project (Prefix.pch).
